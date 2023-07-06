5bb2300f27ddf482d69f822bfa11c658e66b8a9b
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-06 15:54:40 +0530

    feat: update MVP

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import { Request, Response } from "express"
+import express from "express"
+import bodyParser from "body-parser"
+import { Request, Response, NextFunction } from "express"
 import { AppDataSource } from "./data-source"
-import { Routes } from "./routes"
-import { User } from "./entity/User"
+
+import { userRouter } from "./routers/userRouter"
 
 import "dotenv/config";
 import { env } from "../config/server";
@@ -12,44 +12,20 @@ AppDataSource.initialize().then(async () => {
 
     // create express app
     const app = express()
+
     app.use(bodyParser.json())
 
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next)
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result)
-            }
-        })
-    })
-
-    // setup express app here
-    // ...
+    app.use("/user" , userRouter)
 
     // start express server
     app.listen(env.PORT)
 
-    // insert new users for test
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Timber",
-            lastName: "Saw",
-            age: 27
-        })
-    )
+    // Error handling
+    app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+        console.log(err);
+        res.status(err.status || 500).send(err.stack);
+      });
 
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Phantom",
-            lastName: "Assassin",
-            age: 24
-        })
-    )
-
-    console.log(`Express server has started on port ${env.PORT}. Open http://localhost:${env.PORT}/users to see results`)
+    console.log(`Express server has started on port ${env.PORT}. Open http://localhost:${env.PORT}/user to see results`)
 
 }).catch(error => console.log(error))

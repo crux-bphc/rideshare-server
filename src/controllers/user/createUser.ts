25ab6be90fb011eb033ffd64c6c8392ff3855f95
atrey-a <atreyasaurabh@gmail.com>
2023-07-07 14:35:43 +0530

    feat: create the createUser endpoint

--- src/controllers/user/createUser.ts
+++ src/controllers/user/createUser.ts
@@ -1,6 +1,23 @@
 import { Request, Response } from "express";
 import { userRepository } from "../../repositories/userRepository";
+import { User } from "../../entity/User";
+
 
 export const createUser = async (req: Request, res: Response) => {
-  
+  try {
+    const newUser = new User();
+
+    newUser.name = req.body.name;
+    newUser.email = req.body.email;
+    newUser.phNo = req.body.phNo;
+    newUser.batch = req.body.batch;
+
+    await userRepository.save(newUser);
+
+    res.status(200).json("Created user.");
+
+  } catch (err) {
+    res.send(500).json(err);
+  }
+
 };

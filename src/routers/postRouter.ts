a93fc564e6f34c6e1365ef423c25833144f7bbd2
atrey-a <atreyasaurabh@gmail.com>
2023-07-09 22:15:26 +0530

    feat: add createPost endpoint

--- src/routers/postRouter.ts
+++ src/routers/postRouter.ts
@@ -1,5 +1,9 @@
 import * as express from "express"
 
+import { createPost } from "../controllers/post/createPost"
+
 const postRouter = express.Router()
+
+postRouter.post("/create", createPost )
 
 export { postRouter }

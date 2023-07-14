031faeab4dabf93455a54d0359b1726217387352
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-15 00:37:14 +0530

    feat: add validation for findUser

--- src/controllers/user/findUser.ts
+++ src/controllers/user/findUser.ts
@@ -1,6 +1,24 @@
 import { Request, Response } from "express";
 import { userRepository } from "../../repositories/userRepository";
+import { validate } from "../../helpers/zodValidateRequest";
 import { User } from "../../entity/User";
+import { z } from "zod";
+
+const dataSchema = z.object({
+  params: z.object({
+    id: z
+      .string({
+        invalid_type_error: "id not a string",
+        required_error: "id is a required path parameter",
+      })
+      .min(0, {
+        message: "id must be a non-empty string",
+      })
+      .uuid({ message: "id must be a valid uuid" }),
+  }),
+})
+
+export const findUserValidator = validate(dataSchema);
 
 export const findUser = async (req: Request, res: Response) => {
   try {
@@ -18,5 +36,4 @@ export const findUser = async (req: Request, res: Response) => {
   } catch (err) {
     res.status(500).json(err);
   }
-
 };

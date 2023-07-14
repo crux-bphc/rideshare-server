1953cf69084e13e86a088ed8730db761a9175f5b
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-15 00:35:39 +0530

    feat: validate createUser in the router

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,12 +1,12 @@
 import * as express from "express"
 
-import { createUser } from "../controllers/user/createUser"
-import { updateUser } from "../controllers/user/updateUser"
+import { createUser,createUserValidator } from "../controllers/user/createUser"
+import { updateUser,updateUserValidator } from "../controllers/user/updateUser"
 import { findUser } from "../controllers/user/findUser"
 
 const userRouter = express.Router()
 
-userRouter.post("/create", createUser )
+userRouter.post("/create", createUserValidator ,createUser )
 userRouter.put("/update/:id", updateUser )
 userRouter.get("/search/:id" , findUser )
 

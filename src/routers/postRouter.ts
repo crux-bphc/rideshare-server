54e2d283844188043aace813b98b6a37d4b9c4ec
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-25 17:22:45 +0530

    feat: add createJoinRequest to router

--- src/routers/postRouter.ts
+++ src/routers/postRouter.ts
@@ -2,10 +2,12 @@ import * as express from "express"
 
 import { createPost } from "../controllers/post/createPost"
 import { updatePost } from "../controllers/post/updatePost"
+import { createJoinRequest } from "../controllers/post/createJoinRequest"
 
 const postRouter = express.Router()
 
 postRouter.post("/create", createPost)
 postRouter.put("/update/:id", updatePost)
+postRouter.post("/join",createJoinRequest)
 
 export { postRouter }

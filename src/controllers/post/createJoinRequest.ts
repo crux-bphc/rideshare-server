9e31c43cb2641695eb8823838ce35da3c59d745d
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-27 00:19:24 +0530

    feat : add users to join queue

--- src/controllers/post/createJoinRequest.ts
+++ src/controllers/post/createJoinRequest.ts
@@ -8,21 +8,51 @@ import { z } from "zod";
 import { validate } from "../../helpers/zodValidateRequest";
 
 export const createJoinRequest = async (req: Request , res: Response) => {
-    const postId = req.params.postId;
-    const userID = req.body.userId;
+    const postId = req.params.id;
+    const userId = req.body.userId;
 
-    let postOP : User | null = null;
+    let userObj : User | null = null;
     let postObj : Post | null = null;
 
     try {
         postObj = await postRepository
-            .createQueryBuilder("post")
-            .where("post.id = postId" , {postId : postId} )
-            .getOne();
+        .createQueryBuilder("post")
+        .where("post.id = :id", { id: postId })
+        .getOne()
+  
         console.log(postObj)
     } catch (err : any) {
         console.log("No Post Found. Error : " , err.message)
+
         return res.status(500).json({ message: "Internal Server Error"})
+    }
+
+    try {
+        userObj = await userRepository
+            .createQueryBuilder("user")
+            .where("user.id = :userId" , {userId : userId})
+            .getOne()
+        
+        console.log(userObj)
+    } catch (err : any) {
+        console.log("No User Found. Error :" , err.message)
+
+        return res.status(500).json({ message: "Internal Server Error"})
+    }
+
+    try {
+        await postRepository.manager.transaction(
+          async (transactionalEntityManager) => {
+            await transactionalEntityManager
+                .createQueryBuilder()
+                .relation(Post, "participantQueue")
+                .of(postObj)
+                .add(userObj);
+            }
+        )
+    
+    } catch (err : any) {
+    return res.json({ message: "User added to join queue" });
     }
 }
 

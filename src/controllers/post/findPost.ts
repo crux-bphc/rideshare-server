a82864eddab52b64a980c143a7501e455533c3df
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-28 02:37:49 +0530

    feat : add zod validation

--- src/controllers/post/findPost.ts
+++ src/controllers/post/findPost.ts
@@ -2,6 +2,27 @@ import { Request, Response } from "express";
 import { postRepository } from "../../repositories/postRepository";
 import { Post } from "../../entity/Post";
 
+import { z } from "zod";
+import { validate } from "../../helpers/zodValidateRequest";
+
+const dataSchema = z.object({
+    params: z.object({
+        postId: z.object({    
+            postId : z
+            .string({
+                invalid_type_error: "postId not a string",
+                required_error: "postId is a required parameter", 
+            })
+            .min(0, {
+                message: "postId must be a non-empty string",
+              })
+            .uuid({ message: "postId must be a valid uuid" }),
+        })
+    })
+})
+
+export const findPostValidator = validate(dataSchema)
+
 export const findPost = async (req: Request , res: Response) => {
     const postId = req.params.postId;
     

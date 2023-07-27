986e09e76d59325741543a3cadd7ec4e837ca64d
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-28 02:35:55 +0530

    feat : implemet zod validation

--- src/routers/postRouter.ts
+++ src/routers/postRouter.ts
@@ -2,14 +2,14 @@ import * as express from "express"
 
 import { createPost } from "../controllers/post/createPost"
 import { updatePost } from "../controllers/post/updatePost"
-import { createJoinRequest } from "../controllers/post/createJoinRequest"
-import { acceptJoinRequest } from "../controllers/post/acceptJoinRequest"
+import { createJoinRequest, createJoinRequestValidator } from "../controllers/post/createJoinRequest"
+import { acceptJoinRequest, acceptJoinRequestValidator } from "../controllers/post/acceptJoinRequest"
 
 const postRouter = express.Router()
 
-postRouter.post("/create", createPost)
-postRouter.put("/update/:id", updatePost)
-postRouter.post("/join/:postId",createJoinRequest)
-postRouter.post("/accept/:postId",acceptJoinRequest)
+postRouter.post("/create",createPost)
+postRouter.put("/update/:id",updatePost)
+postRouter.post("/join/:postId",createJoinRequestValidator ,createJoinRequest)
+postRouter.post("/accept/:postId",acceptJoinRequestValidator , acceptJoinRequest)
 
 export { postRouter }

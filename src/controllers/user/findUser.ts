353b6a630319c67a760e8bffe4330690b2c4b511
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-28 02:18:57 +0530

    feat : select posts and postRequests when querying for user

--- src/controllers/user/findUser.ts
+++ src/controllers/user/findUser.ts
@@ -5,7 +5,7 @@ import { User } from "../../entity/User";
 import { z } from "zod";
 
 const dataSchema = z.object({
-  params: z.object({
+  query: z.object({
     id: z
       .string({
         invalid_type_error: "id not a string",
@@ -21,19 +21,24 @@ const dataSchema = z.object({
 export const findUserValidator = validate(dataSchema);
 
 export const findUser = async (req: Request, res: Response) => {
+
+  let userObj : User | null = null;
+  
   try {
-    const user: User = await userRepository
-      .createQueryBuilder("user")
-      .where("user.id = :id", { id: req.params.id })
-      .getOne()
+    userObj = await userRepository
+                .createQueryBuilder("user")
+                .where("user.id = :id", { id: req.query.id})
+                .leftJoinAndSelect("user.postRequests", "postRequests")
+                .leftJoinAndSelect("user.posts", "posts")
+                .getOne()
 
-    if (!user) {
-      return res.status(403).json({ message: "User doesn't exist" });
+    if (!userObj) {
+      return res.status(404).json({ message: "User not found in DB" });
     }
-
-    return res.status(200).json(user);
-
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    
+  } catch(err : any){
+    console.log("Error while querying for User. Error : ", err.message)
+    res.status(500).json({ message: "Internal Server Error" });
+}
+  return res.status(200).json(userObj);
 };

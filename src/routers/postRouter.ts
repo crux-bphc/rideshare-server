6728ed6c546751e24a8921c74f6e8fb2345c155b
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-28 02:21:14 +0530

    feat : add accept request endpoint

--- src/routers/postRouter.ts
+++ src/routers/postRouter.ts
@@ -3,11 +3,13 @@ import * as express from "express"
 import { createPost } from "../controllers/post/createPost"
 import { updatePost } from "../controllers/post/updatePost"
 import { createJoinRequest } from "../controllers/post/createJoinRequest"
+import { acceptJoinRequest } from "../controllers/post/acceptJoinRequest"
 
 const postRouter = express.Router()
 
 postRouter.post("/create", createPost)
 postRouter.put("/update/:id", updatePost)
-postRouter.post("/join/:id",createJoinRequest)
+postRouter.post("/join/:postId",createJoinRequest)
+postRouter.post("/accept/:postId",acceptJoinRequest)
 
 export { postRouter }

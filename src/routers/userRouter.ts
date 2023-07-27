67c28e5ad662d655312ce6da54f3da00d625155f
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-28 02:21:38 +0530

    feat : add userID as a query param

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -2,12 +2,12 @@ import * as express from "express"
 
 import { createUser, createUserValidator } from "../controllers/user/createUser"
 import { updateUser, updateUserValidator } from "../controllers/user/updateUser"
-import { findUser } from "../controllers/user/findUser"
+import { findUser, findUserValidator } from "../controllers/user/findUser"
 
 const userRouter = express.Router()
 
 userRouter.post("/create", createUserValidator, createUser)
 userRouter.put("/update/:id", updateUserValidator, updateUser)
-userRouter.get("/search/:id", findUser)
+userRouter.get("/search",findUserValidator ,findUser)
 
 export { userRouter }

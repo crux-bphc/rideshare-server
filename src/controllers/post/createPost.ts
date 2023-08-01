a707448d6ee8a3d8b7eb216ef218437a1abd8be8
atrey-a <atreyasaurabh@gmail.com>
2023-08-01 23:40:49 +0530

    feat: add validation to the createPost endpoint

--- src/controllers/post/createPost.ts
+++ src/controllers/post/createPost.ts
@@ -3,7 +3,75 @@ import { postRepository } from "../../repositories/postRepository";
 import { Post } from "../../entity/Post";
 import { userRepository } from "../../repositories/userRepository";
 import { User } from "../../entity/User";
+import { Place } from "../../helpers/places";
+import { z } from "zod";
+import { validate } from "../../helpers/zodValidateRequest";
 
+const dataSchema = z.object({
+  body: z.object({
+    userId: z
+      .string({
+        invalid_type_error: "userId not a string",
+        required_error: "userId is a required parameter",
+      })
+      .min(0, {
+        message: "userId must be a non-empty string",
+      })
+      .uuid({
+        message: "userId must be a valid uuid"
+      }),
+
+    fromPlace: z
+      .nativeEnum(Place, {
+        invalid_type_error: "fromPlace must be a valid enum of the defined places"
+      }),
+
+    toPlace: z
+      .nativeEnum(Place, {
+        invalid_type_error: "toPlace must be a valid enum of the defined places"
+      }),
+
+    seats: z
+      .number({
+        invalid_type_error: "seats must be a number",
+        required_error: "seats is a required parameter",
+      })
+      .int({
+        message: "seats must be an integer",
+      })
+      .positive({
+        message: "seats must be a positive integer"
+      }),
+
+    timeRangeStart: z
+      .coerce.date({
+        invalid_type_error: "timeRangeStart must be a Date() object"
+      })
+      .min(new Date(), {
+        message: "timeRangeStart must occur after the time of posting"
+      }),
+
+    timeRangeStop: z
+      .coerce.date({
+        invalid_type_error: "timeRangeStop must be a Date() object"
+      })
+      .min(new Date(), {
+        message: "timeRangeStop must occur after the time of posting"
+      }),
+
+    description: z
+      .string({
+        invalid_type_error: "description must be a valid string"
+      })
+      .optional(),
+
+  })
+  .refine(data => new Date(data.timeRangeStart) < new Date(data.timeRangeStop),
+    "timeRangeStart must occur before timeRangeStop",
+  )
+})
+
+export const createPostValidator = validate(dataSchema)
 
 export const createPost = async (req: Request, res: Response) => {
   try {
@@ -14,9 +82,9 @@ export const createPost = async (req: Request, res: Response) => {
       .getOne()
 
     if (!user) {
-      return res.status(403).json({ message: "User id invalid" });
+      return res.status(403).json({ message: "User not found!" });
     }
-    
+
     console.log(user)
     const currentDateTime: Date = new Date();
 
--- src/routers/postRouter.ts
+++ src/routers/postRouter.ts
@@ -1,6 +1,6 @@
 import * as express from "express"
 
-import { createPost } from "../controllers/post/createPost"
+import { createPost, createPostValidator } from "../controllers/post/createPost"
 import { updatePost } from "../controllers/post/updatePost"
 import { createJoinRequest, createJoinRequestValidator } from "../controllers/post/createJoinRequest"
 import { acceptJoinRequest, acceptJoinRequestValidator } from "../controllers/post/acceptJoinRequest"
@@ -9,7 +9,7 @@ import { searchPostValidator, searchPosts } from "../controllers/post/searchPost
 
 const postRouter = express.Router()
 
-postRouter.post("/create",createPost)
+postRouter.post("/create",createPostValidator,createPost)
 postRouter.put("/update/:id",updatePost)
 postRouter.post("/join/:postId",createJoinRequestValidator ,createJoinRequest)
 postRouter.post("/accept/:postId",acceptJoinRequestValidator , acceptJoinRequest)

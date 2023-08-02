41a1450f6253f9e8fd80c8bac1482a4db34bd8a5
atrey-a <atreyasaurabh@gmail.com>
2023-08-03 01:25:41 +0530

    fix: correct uniqueness constraint in createUser to throw 400 instead of 500

--- src/controllers/user/createUser.ts
+++ src/controllers/user/createUser.ts
@@ -69,6 +69,9 @@ export const createUser = async (req: Request, res: Response) => {
 
   } catch (err) {
     console.log("Error creating user:", err.message)
+    if (err.message .substring(0,46) == "duplicate key value violates unique constraint") {
+      return res.status(400).json({ message: "Email or Phone Number already exists" })
+    }
     return res.status(500).json({ message: "Internal Server Error" });
   }
 

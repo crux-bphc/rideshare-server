9d736eb7f266712dfd05c87a5fe86f2fb36c4424
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-08-15 15:58:57 +0530

    feat : add auth check for update user

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,15 +1,23 @@
-import * as express from "express"
+import * as express from "express";
 
-import { createUser, createUserValidator } from "../controllers/user/createUser"
-import { updateUser, updateUserValidator } from "../controllers/user/updateUser"
-import { findUser, findUserValidator } from "../controllers/user/findUser"
-import { loginUser, loginUserValidator } from "../controllers/user/loginUser"
+import {
+  createUser,
+  createUserValidator,
+} from "../controllers/user/createUser";
+import {
+  updateUser,
+  updateUserValidator,
+} from "../controllers/user/updateUser";
+import { findUser, findUserValidator } from "../controllers/user/findUser";
+import { loginUser, loginUserValidator } from "../controllers/user/loginUser";
 
-const userRouter = express.Router()
+import { isLoggedIn } from "../middleware/auth";
 
-userRouter.post("/create", createUserValidator, createUser)
-userRouter.put("/update/:id", updateUserValidator, updateUser)
-userRouter.get("/search", findUserValidator, findUser)
-userRouter.post("/login", loginUserValidator, loginUser)
+const userRouter = express.Router();
 
-export { userRouter }
+userRouter.post("/create", createUserValidator, createUser);
+userRouter.put("/update/:id", updateUserValidator, isLoggedIn, updateUser);
+userRouter.get("/search", findUserValidator, findUser);
+userRouter.post("/login", loginUserValidator, loginUser);
+
+export { userRouter };

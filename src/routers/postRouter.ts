1d4cd0989b38763bb10d45662e48eda1c53c1e8b
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-08-15 16:01:41 +0530

    feat : add auth check for post routes

--- src/routers/postRouter.ts
+++ src/routers/postRouter.ts
@@ -1,19 +1,44 @@
-import * as express from "express"
+import * as express from "express";
 
-import { createPost, createPostValidator } from "../controllers/post/createPost"
-import { updatePost, updatePostValidator } from "../controllers/post/updatePost"
-import { createJoinRequest, createJoinRequestValidator } from "../controllers/post/createJoinRequest"
-import { acceptJoinRequest, acceptJoinRequestValidator } from "../controllers/post/acceptJoinRequest"
-import { findPost, findPostValidator } from "../controllers/post/findPost"
-import { searchPosts, searchPostValidator } from "../controllers/post/searchPosts"
+import {
+  createPost,
+  createPostValidator,
+} from "../controllers/post/createPost";
 
-const postRouter = express.Router()
+import {
+  updatePost,
+  updatePostValidator,
+} from "../controllers/post/updatePost";
 
-postRouter.post("/create", createPostValidator, createPost)
-postRouter.put("/update/:id", updatePostValidator, updatePost)
-postRouter.post("/join/:postId", createJoinRequestValidator, createJoinRequest)
-postRouter.post("/accept/:postId", acceptJoinRequestValidator, acceptJoinRequest)
-postRouter.get("/find/:postId", findPostValidator, findPost)
-postRouter.get("/search", searchPostValidator, searchPosts)
+import {
+  createJoinRequest,
+  createJoinRequestValidator,
+} from "../controllers/post/createJoinRequest";
 
-export { postRouter }
+import {
+  acceptJoinRequest,
+  acceptJoinRequestValidator,
+} from "../controllers/post/acceptJoinRequest";
+
+import { 
+    findPost, 
+    findPostValidator 
+} from "../controllers/post/findPost";
+
+import {
+  searchPosts,
+  searchPostValidator,
+} from "../controllers/post/searchPosts";
+
+import { isLoggedIn } from "../middleware/auth";
+
+const postRouter = express.Router();
+
+postRouter.post("/create", createPostValidator,isLoggedIn ,createPost);
+postRouter.put("/update/:id", updatePostValidator,isLoggedIn ,updatePost);
+postRouter.post("/join/:postId", createJoinRequestValidator,isLoggedIn ,createJoinRequest);
+postRouter.post("/accept/:postId", acceptJoinRequestValidator,isLoggedIn,acceptJoinRequest);
+postRouter.get("/find/:postId", findPostValidator,isLoggedIn ,findPost);
+postRouter.get("/search", searchPostValidator,isLoggedIn ,searchPosts);
+
+export { postRouter };

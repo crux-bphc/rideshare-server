d2f5978a31bb48a8723c5b940778fabf5cca807c
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-08-16 09:24:51 +0530

    feat : switch entity to ride

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "./entity/User";
-import { Post } from "./entity/Post";
+import { Ride } from "./entity/Ride";
 import { env } from "../config/server";
 
 export const AppDataSource = new DataSource({
@@ -9,7 +9,7 @@ export const AppDataSource = new DataSource({
   url: `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@db:5432?db=${env.POSTGRES_DB}`,
   synchronize: true,
   logging: false,
-  entities: [User, Post],
+  entities: [User, Ride],
   migrations: [],
   subscribers: [],
 });

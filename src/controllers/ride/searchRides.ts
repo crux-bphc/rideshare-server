fa7978a942b01756ae5148708d922516b171a68b
atrey-a <atreyasaurabh@gmail.com>
2023-08-22 16:06:36 +0530

    fix: coerce query values from string in searchRides

--- src/controllers/ride/searchRides.ts
+++ src/controllers/ride/searchRides.ts
@@ -8,16 +8,28 @@ import { validate } from "../../helpers/zodValidateRequest";
 const dataSchema = z.object({
   query: z.object({
     fromPlace: z
-      .nativeEnum(Place, {
+    .preprocess(
+      (input) => {
+        const processed = z.string().regex(/^\d+$/).transform(Number).safeParse(input);
+        return processed.success ? processed.data : input;
+      },
+      z.nativeEnum(Place, {
         invalid_type_error: "fromPlace must be a valid enum of the defined places"
       })
       .optional(),
+    ),
 
     toPlace: z
-      .nativeEnum(Place, {
+    .preprocess(
+      (input) => {
+        const processed = z.string().regex(/^\d+$/).transform(Number).safeParse(input);
+        return processed.success ? processed.data : input;
+      },
+      z.nativeEnum(Place, {
         invalid_type_error: "toPlace must be a valid enum of the defined places"
       })
       .optional(),
+    ),
 
     startTime: z
       .coerce.date({
@@ -32,6 +44,7 @@ const dataSchema = z.object({
       .optional(),
 
     availableSeats: z
+      .coerce
       .number({
         invalid_type_error: "availableSeats must be an integer"
       })
@@ -44,12 +57,14 @@ const dataSchema = z.object({
       .optional(),
 
     activeRides: z
+      .coerce
       .boolean({
         invalid_type_error: "activeRides must be a boolean"
       })
       .optional(),
 
     startAtRide: z
+      .coerce
       .number({
         invalid_type_error: "startAtRide must be an integer"
       })
@@ -62,6 +77,7 @@ const dataSchema = z.object({
       .optional(),
 
     endAtRide: z
+      .coerce
       .number({
         invalid_type_error: "endAtRide must be an integer"
       })
@@ -74,6 +90,7 @@ const dataSchema = z.object({
       .optional(),
 
     orderBy: z
+      .coerce
       .number({
         invalid_type_error: "orderBy must be an integer"
       })
@@ -110,26 +127,27 @@ let orderingAlong: object = {
 
 export const searchRides = async (req: Request, res: Response) => {
 
-  let fromPlace: Place | null = req.query.fromPlace;
-  let toPlace: Place | null = req.query.toPlace;
-  let startTime: Date | null = req.query.startTime; // Renders trips whose timeRange is within or after startTime
-  let endTime: Date | null = req.query.endTime; // Renders trips whose timeRange is within or before endTime
+  let fromPlace: Place | null = req.query.fromPlace != null ? parseInt(req.query.fromPlace) : null;
+  let toPlace: Place | null = req.query.toPlace != null ? parseInt(req.query.toPlace) : null;
+  let startTime: Date | null = req.query.startTime != null ? new Date(req.query.startTime) : null; // Renders trips whose timeRange is within or after startTime
+  let endTime: Date | null = req.query.endTime != null ? new Date(req.query.endTime) : null; // Renders trips whose timeRange is within or before endTime
   // Use 1 or more here, to show only those rides which have available seats. leaving empty renders all rides without checking seats
-  let availableSeats: number | null = req.query.availableSeats;
+  let availableSeats: number | null = req.query.availableSeats != null ? parseInt(req.query.availableSeats) : null;
   // true renders rides whose trips are yet to start. false renders trips which have started/finished in the past. leaving empty renders all rides regardless.
-  let activeRides: boolean | null = req.query.activeRides;
+  let activeRides: boolean | null = req.query.activeRides != null ? Boolean(req.query.activeRides.toLowerCase() === "true") : null;
   // Pagination - both numbers inclusive
-  let startAtRide: number = req.query.startAtRide || 1;
-  let endAtRide: number = req.query.endAtRide || 10;
+  let startAtRide: number = req.query.startAtRide != null ? parseInt(req.query.startAtRide) : 1;
+  let endAtRide: number = req.query.endAtRide != null ? parseInt(req.query.endAtRide) : 10;
   // orderBy = 1 renders rides sorted by time of posting. orderBy = 2 renders rides sorted by time of departure. orderBy = 3 renders rides sorted by number of seats available.
   // the corresponding negative numbers renders rides in descending order
-  let orderBy: number = req.query.orderBy || 1;
+  let orderBy: number = req.query.orderBy != null ? parseInt(req.query.orderBy) : 1;
 
   let searchFilter: string = ""
   let searchObj: object = {}
 
   if (availableSeats != null) {
-    searchFilter = searchFilter + " AND (ride.seats - (SELECT COUNT(participant) FROM UNNEST(ride.participants) AS participant)) >= :availableSeats";
+    searchFilter = searchFilter + " AND ((ride.seats) - (SELECT COUNT(participant) FROM UNNEST(participants) AS participant))) >= :availableSeats";
+    // searchFilter = searchFilter + " AND (ride.seats - ride.participantCount) >= :availableSeats"
     searchObj["availableSeats"] = availableSeats;
   }
 
@@ -177,8 +195,7 @@ export const searchRides = async (req: Request, res: Response) => {
       .getMany()
 
   } catch (err: any) {
-    // console.log("Error while searching DB for rides.", err.message)
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ message: "Internal Server Error", "error": err });
   }
 
   res.status(200).json(rides);

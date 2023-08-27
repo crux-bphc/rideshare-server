260991a9733e5b226a20609d4140bd703bf1c663
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-08-27 17:39:19 +0530

    feat : handle errors

--- src/helpers/googleIdVerify.ts
+++ src/helpers/googleIdVerify.ts
@@ -1,16 +1,20 @@
 import "dotenv/config";
 import { env } from "../../config/server";
-import {OAuth2Client} from "google-auth-library"
+import { OAuth2Client } from "google-auth-library";
 
 const client = new OAuth2Client();
 
-export const verify  = async(token : string) => {
-  const ticket = await client.verifyIdToken({
+export const verify = async (token: string) => {
+  try {
+    const ticket = await client.verifyIdToken({
       idToken: token,
-      audience: env.GOOGLE_CLIENT_ID, 
-  });
-  const payload = ticket.getPayload();
-  const userid = payload['sub'];
-  const domain = payload['hd'];
-  console.log(payload)
-}
+      audience: env.GOOGLE_CLIENT_ID,
+    });
+    const payload = ticket.getPayload();
+    const userid = payload["sub"];
+    const domain = payload["hd"];
+    console.log(payload);
+  } catch (err: any) {
+    console.log("Error while verifying Google Token. Error : ", err.message);
+  }
+};

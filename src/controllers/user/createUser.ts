827a4df69f4c1a82d438de7856fd1b69d0484d4d
atrey-a <atreyasaurabh@gmail.com>
2023-08-31 18:39:58 +0530

    refactor: remove unnecessary return of userObj in createUser

--- src/controllers/user/createUser.ts
+++ src/controllers/user/createUser.ts
@@ -55,9 +55,7 @@ export const createUser = async (req: Request, res: Response) => {
       .returning("*")
       .execute()
 
-    const user = newUser.generatedMaps[0] as User;
-
-    return res.status(201).json({ "message": "Created user.", "newUser": user });
+    return res.status(201).json({ message: "Created user." });
 
   } catch (err) {
     if (err.code == "23505") {

e1f47d3f40e6d694a976abe7f6c96abe6eb02478
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-09-05 23:57:26 +0530

    chore: update error message

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -7,17 +7,17 @@ import { env } from "../../config/server";
 export const isLoggedIn = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const token = req.header('Authorization')?.replace('Bearer ', '');
-    const secretKey = env.JWT_SECRET;
+    const accessSecretKey = env.ACCESS_JWT_SECRET;
 
     if (!token) {
       throw new Error();
     }
 
-    const decoded = jwt.verify(token, secretKey);
+    const decoded = jwt.verify(token, accessSecretKey);
     req.token = decoded;
 
     next();
   } catch (err) {
-    res.status(401).send('User not logged in');
+    res.status(401).send('Token expired/User not logged in.');
   }
 }

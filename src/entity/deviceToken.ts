98b691d3d15d5dab5700b981e7b87f55da49820b
palaniappan-r <palaniappan.r.mail@gmail.com>
2024-01-02 15:22:25 +0530

    feat: add deviceToken entity class

---
+++ src/entity/deviceToken.ts
@@ -0,0 +1,26 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+} from "typeorm"
+import { User } from "./User"
+
+
+@Entity()
+export class deviceToken {
+
+  @PrimaryGeneratedColumn("uuid")
+  id: string
+
+  @ManyToOne(() => User)
+  user: User;
+
+  @Column({
+    type: "varchar",
+    width: 200,
+    unique: true,
+    nullable: false
+  })
+  tokenId: string
+}

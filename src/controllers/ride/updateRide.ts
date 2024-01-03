9d0efdda2c7408ee13cd520937172573155fb082
Saurabh Atreya <atreyasaurabh@gmail.com>
2024-01-03 20:04:54 +0530

    feat: add notifs when OP edits ride

--- src/controllers/ride/updateRide.ts
+++ src/controllers/ride/updateRide.ts
@@ -6,6 +6,9 @@ import { User } from "../../entity/User";
 import { Place } from "../../helpers/places";
 import { z } from "zod";
 import { validate } from "../../helpers/zodValidateRequest";
+import { deviceToken } from "../../entity/deviceToken";
+import { deviceTokenRepository } from "../../repositories/deviceTokenRepository";
+import { messaging } from "../../helpers/firebaseMessaging";
 
 const dataSchema = z.object({
   params: z.object({
@@ -127,7 +130,9 @@ export const updateRide = async (req: Request, res: Response) => {
   try {
     rideObj = await rideRepository
       .createQueryBuilder("ride")
+      .leftJoinAndSelect("ride.participantQueue", "participantQueue")
       .leftJoinAndSelect("ride.originalPoster", "originalPoster")
+      .leftJoinAndSelect("ride.participants", "participants")
       .where("ride.id = :id", { id: rideId })
       .getOne();
   } catch (err) {
@@ -182,7 +187,7 @@ export const updateRide = async (req: Request, res: Response) => {
         seats: req.body.seats,
         timeRangeStart: req.body.timeRangeStart,
         timeRangeStop: req.body.timeRangeStop,
-        status: req.body.deleteRide,
+        status: req.body.updateRide,
         updatedAt: currentDateTime,
         description: req.body.description,
       })
@@ -191,6 +196,99 @@ export const updateRide = async (req: Request, res: Response) => {
   } catch (err) {
     console.log("[updateRide.ts] Error in updating ride in db: ", err.message);
     return res.send(500).json({ message: "Internal Server Error!" });
+  }
+
+  const joinedUserIds = new Set(rideObj.participants.map((user) => user.id));
+  joinedUserIds.delete(userId);
+
+  if (joinedUserIds.size > 0) {
+    let joinedDeviceObjs: deviceToken[];
+    try {
+      joinedDeviceObjs = await deviceTokenRepository
+        .createQueryBuilder("deviceToken")
+        .select("deviceToken.tokenId")
+        .where("deviceToken.user.id IN (:...userIds)", {
+          userIds: Array.from(joinedUserIds),
+        })
+        .getMany();
+    } catch (err) {
+      console.log(
+        "[updateRide.ts] Error in finding deviceTokens of participants from db: ",
+        err.message
+      );
+      return res.status(500).json({ message: "Internal Server Error!" });
+    }
+
+    const joinedUsersPayload = {
+      notification: {
+        title: `${rideObj.originalPoster.name} Updated The Ride You Were Accepted Into`,
+        body: "View the ride for more details.",
+      },
+      data: {
+        action: "rideDeleted",
+        userName: rideObj.originalPoster.name,
+        userId: rideObj.originalPoster.id,
+        rideId: rideId,
+      },
+      tokens: joinedDeviceObjs.map((deviceToken) => deviceToken.tokenId),
+    };
+
+    try {
+      messaging.sendEachForMulticast(joinedUsersPayload);
+    } catch (err) {
+      console.log(
+        "[updateRide.ts] Error in sending notifications to participants: ",
+        err.message
+      );
+      return res.status(500).json({ message: "Internal Server Error!" });
+    }
+  }
+
+  const requestedUserIds = new Set(
+    rideObj.participantQueue.map((user) => user.id)
+  );
+
+  if (requestedUserIds.size > 0) {
+    let requestedDeviceObjs: deviceToken[];
+    try {
+      requestedDeviceObjs = await deviceTokenRepository
+        .createQueryBuilder("deviceToken")
+        .select("deviceToken.tokenId")
+        .where("deviceToken.user.id IN (:...userIds)", {
+          userIds: Array.from(requestedUserIds),
+        })
+        .getMany();
+    } catch (err) {
+      console.log(
+        "[updateRide.ts] Error in finding deviceTokens of participantQueue from db: ",
+        err.message
+      );
+      return res.status(500).json({ message: "Internal Server Error!" });
+    }
+
+    const requestedUsersPayload = {
+      notification: {
+        title: `${rideObj.originalPoster.name} Updated The Ride You Requested To Join`,
+        body: "View the ride for more details.",
+      },
+      data: {
+        action: "rideDeleted",
+        userName: rideObj.originalPoster.name,
+        userId: rideObj.originalPoster.id,
+        rideId: rideId,
+      },
+      tokens: requestedDeviceObjs.map((deviceToken) => deviceToken.tokenId),
+    };
+
+    try {
+      messaging.sendEachForMulticast(requestedUsersPayload);
+    } catch (err) {
+      console.log(
+        "[updateRide.ts] Error in sending notifications to participantQueue: ",
+        err.message
+      );
+      return res.status(500).json({ message: "Internal Server Error!" });
+    }
   }
 
   return res.status(200).json({ message: "Updated ride." });

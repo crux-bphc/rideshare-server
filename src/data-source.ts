fa73ec002682686a34a53e3292fafdbcca9b5097
Shre-D <shreyash1264@gmail.com>
2024-04-10 04:22:27 +0530

    feat: accomodate for different env files in different environments

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,12 +5,32 @@ import { Ride } from "./entity/Ride";
 import { deviceToken } from "./entity/deviceToken";
 import { env } from "../config/server";
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  url: `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@db:${env.PGPORT}?db=${env.POSTGRES_DB}`,
-  synchronize: true,
-  logging: false,
-  entities: [User, Ride, deviceToken],
-  migrations: [],
-  subscribers: [],
-});
+export let AppDataSource: DataSource = null;
+
+if (env.NODE_ENV == "development") {
+  AppDataSource = new DataSource({
+    type: "postgres",
+    url: `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@db-dev:${env.PGPORT}?db-dev=${env.POSTGRES_DB}`,
+    synchronize: true,
+    logging: false,
+    entities: [User, Ride, deviceToken],
+    migrations: [],
+    subscribers: [],
+  });
+}
+else if (env.NODE_ENV == "production") {
+  AppDataSource = new DataSource({
+    type: "postgres",
+    url: `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@db:${env.PGPORT}?db=${env.POSTGRES_DB}`,
+    synchronize: true,
+    logging: false,
+    entities: [User, Ride, deviceToken],
+    migrations: [],
+    subscribers: [],
+  });
+}
+else {
+  console.error("Please enter a valid environment!")
+}
+
+
